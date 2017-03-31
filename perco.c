2ac24903beb2bf10336f84a46c387aebf47189d7
Lucía Fabio <luciaa.fabio@gmail.com>
2017-03-31 11:08:46 -0300

    update

--- perco.c
+++ perco.c
@@ -5,10 +5,11 @@
 
 int main(int argc, char *argv[]) {
   double threshold = strtod(argv[1], NULL); //probability of site being occuppied
-  int netw[5][5];
+  int n = atoi(argv[2]);
+  int netw[n][n];
   srand(time(NULL));
-  for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++) {
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++) {
 
       double r = 2*((double)rand())/RAND_MAX; //generate random number between 0 and 1
 
