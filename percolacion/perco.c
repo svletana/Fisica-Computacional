cb01a55ba35322b9e703160dbbba6f204d6cce81
micaela mosquera panizo <mmosquera@ws5.labo5>
2017-04-18 16:07:01 -0300

    comentarios del director

--- percolacion/perco.c
+++ percolacion/perco.c
@@ -26,7 +26,7 @@ int main(int argc,char *argv[])
      }
 
   red=(int *)malloc(n*n*sizeof(int));
-
+/*
   FILE *output = fopen("output.txt", "w");
   if (output == NULL)
   {
@@ -35,24 +35,24 @@ int main(int argc,char *argv[])
   }
 
   fprintf(output, "###INICIO ITERACIONES###\n##### dimension: %dx%d , iteraciones z: %d , P = %d #####\n\n",n,n,z,p);
-
+*/
   float promedio = 0;
-
-  for(i=0;i<z;i++)
+  dp = 0.001
+  for(i=0;i<z;i++) //MODIFICAR COMO ESCRIBIMOS A UN ARCHIVO
     {
           srand(time(NULL));
-          prob=0.4;
+          prob=0.4+i*dp;
           denominador=2.0;
           pc=prob;
-          for(j=0;j<P;j++)
+          for(j=0;j<P;j++) //sacarlo para el punto 2
           {
               llenar(red,n,prob); //llena red
               imprimir(red,n);
-              hoshen(red,n); //etiqueta red
+              hoshen(red,n); //etiqueta red, corre funcion masa y me da s y clase
               imprimir(red,n);
 
               denominador = 2.0*denominador;
-
+		/* //solo quiero que etiqueta percola, este loop no
               if (percola(red,n))
               {
                 //printf("percola a proba: %.5f\n",prob);
@@ -60,18 +60,18 @@ int main(int argc,char *argv[])
                 prob+=(-1.0/denominador);
               }
               else { prob+=(1.0/denominador); }
-
+		*/
           }
           promedio+=pc;
-          fprintf(output, "%.5f\n", pc);
+          //fprintf(output, "%.5f\n", pc);
           if (i%100==0) { printf("iteracion actual: %d\n pc: %.5f\n",i,pc); }
     }
 
-  printf("\npromedio: %.5f\n",promedio/z);
-  fprintf(output,"\npromedio: %.5f\n",promedio/z);
+  //printf("\npromedio: %.5f\n",promedio/z);
+  //fprintf(output,"\npromedio: %.5f\n",promedio/z);
 
   free(red);
-  fclose(output);
+  //fclose(output);
 
   return 0;
 }
