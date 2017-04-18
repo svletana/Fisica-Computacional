0d16959019650d3275a81b58a8d1dbc1ffd2540b
micaela mosquera panizo <mmosquera@ws5.labo5>
2017-04-18 15:51:19 -0300

    masa de cada cluster

--- percolacion/masa.c
+++ percolacion/masa.c
@@ -5,46 +5,34 @@
 
 #include "functions.h"
 
-//no parece funcionar bien si hay un solo cluster, o sea una sola etiqueta, pero es muy poco probable que eso pase creo...
 
 int masa(int *red, int *clase, int n)
 {
-  int i,j,k,q,*masas, *labels;
+  int i,j,*s;
+  s=(int *)malloc(n*n*sizeof(int));
 
-  labels = (int *)malloc(0.5*n*n*sizeof(int));
-  for(i=0;i<0.5*n*n;i++) *(labels + i) = -1;
+  FILE *etiquetas = fopen("claseYs.txt","w");
 
-  j = 0;
+  for(i=0;i<n*n;i++) *(s+i) = 0;
 
-  for(i=1;i<n*n;i++)
+  for (i=0;i<n*n;i++) {
+	for (j=0;j<n*n;j++) {
+		if (clase[i]==red[j] && clase[i]>1) {s[i]+=1;}
+		}
+	}
+
+  fprintf(etiquetas,"vector s\n");
+  for(i=0;i<n*n;i++) 
   {
-    if(*(clase + i)>1)
-    {
-      *(labels + i - 1) = *(clase + i); //label 'i' en el lugar i!!
-      //printf("label array: %d\n",*(labels + i - 1)); //debug
-      j++;
-    }
+    fprintf(etiquetas, "%d,",*(s+i));
   }
-
-  //for(i=0;i<0.5*n*n;i++) printf("cosa: %d\n", *(labels + i));;
-
-  //printf("counter: %d\n", j); //debug
-
-  masas = (int *)malloc(j*sizeof(int));
-
-  for(i=0;i<0.5*n*n;i++)
+  fprintf(etiquetas,"\nvector clase\n");
+  for(i=0;i<n*n;i++) 
   {
-    q = *(labels + i);
-    if(q<0) pass;
-    else {
-      for(k=0;k<n*n;k++)
-      {
-        if(*(red + k)==q)
-        {
-          //contar
-        }
-      }
-    }
+    fprintf(etiquetas, "%d,",*(clase+i));
   }
+ 
+  fclose(etiquetas);
 
+  return 0;
 }
